b3aa864b559260a3c8b5133fb8966b325d05633f
Akhmad Adrian Adrimas <adrianbabame@gmail.com>
2019-04-26 15:38:41 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import TotalIuran from './Components/Iuran'
+import TenagaKerja from './Components/TenagaKerja'
 
 class App extends Component {
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      autoplaySpeed: 3000
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Slider {...settings}>
+          <div>
+            <TotalIuran />
+          </div>
+          <div>
+            <TenagaKerja />
+          </div>
+        </Slider>
       </div>
     );
   }
---
+++ src/Components/TenagaKerja.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+import { Bar } from 'react-chartjs-2'
+import { Container, Grid, Card } from 'semantic-ui-react'
+import axios from 'axios'
+import moment from 'moment'
+import localization from 'moment/locale/id'
+
+class TenagaKerja extends Component {
+  constructor() {
+    super()
+    this.state = {
+      chartData: {},
+      chartOptions: {},
+      totalTk: 0,
+      lastMonth: '',
+      lastTk: 0
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData()
+  }
+
+  getChartData(){
+    axios.get('http://127.0.0.1:8000/api/tk/')
+      .then(res => {
+        const allTks = res.data
+        const jumlah = allTks.map(allTk => allTk.jml_tk)
+        const bulan = allTks.map(allTk => moment(allTk.month).locale('id', localization).format('MMMM'))
+        const add = (a, b) => a + b
+        const totalTk = jumlah.reduce(add)
+        const lastMonth = bulan[bulan.length - 1]
+        const lastTk = jumlah[jumlah.length - 1]
+        this.setState({
+          chartData: {
+            labels: bulan,
+            datasets: [
+              {
+                label: 'Penambahan Tenaga Kerja',
+                borderColor: 'rgba(255,99,132,1)',
+                backgroundColor: [
+	                'rgba(255, 99, 132, 0.4)',
+	                'rgba(54, 162, 235, 0.4)',
+	                'rgba(255, 206, 86, 0.4)',
+	                'rgba(75, 192, 192, 0.4)',
+	                'rgba(153, 102, 255, 0.4)',
+	                'rgba(255, 159, 64, 0.4)'
+           		  ],
+                borderWidth: 1,
+                data: jumlah
+              }
+            ]
+          },
+          chartOptions: {
+            maintainAspectRatio: false,
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero:true,
+                  callback: function(value, index, values) {
+                    return value.toLocaleString('en');
+                  },
+                }
+              }]
+            }
+          },
+          totalTk: totalTk.toLocaleString('id'),
+          lastMonth,
+          lastTk
+        })
+      })
+
+  }
+
+  render() {
+    return(
+      <Container style={{ marginTop: '7em' }}>
+        <Grid centered verticalAlign='middle' style={{ height: '100%' }}>
+          <Grid.Row>
+            <Card.Group>
+              <Card raised>
+                <Card.Content>
+                  <Card.Header>Total Penambahan TK</Card.Header>
+                  <Card.Description style={{fontSize: '2em'}}>{this.state.totalTk}</Card.Description>
+                </Card.Content>
+              </Card>
+              <Card raised>
+                <Card.Content>
+                  <Card.Header>Penambahan {this.state.lastMonth}</Card.Header>
+                  <Card.Description style={{fontSize: '2em'}}>{this.state.lastTk.toLocaleString('id')}</Card.Description>
+              </Card.Content>
+              </Card>
+            </Card.Group>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <div>
+                <Bar
+                  data={this.state.chartData}
+                  width={100}
+                  height={50}
+                  options={this.state.chartOptions}/>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    )
+  }
+}
+
+export default TenagaKerja
