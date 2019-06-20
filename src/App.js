929da63c10ddec51eb1dd2f3a73c0ba0b06794d3
Akhmad Adrian Adrimas <adrianbabame@gmail.com>
2019-06-20 23:09:01 +0700

    Move fetching data function to parent

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ import {
   Visibility,
   Segment,
 } from 'semantic-ui-react'
+import axios from 'axios'
+import moment from 'moment'
+import localization from 'moment/locale/id'
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { bold } from 'ansi-colors';
 
 const getWidth = () => {
   const isSSR = typeof window === 'undefined'
@@ -20,13 +25,94 @@ const getWidth = () => {
   return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
 }
 
+const createChartData = (labels, label, data) => {
+  return {
+    labels: labels,
+      datasets: [
+        {
+          label: label,
+          borderColor: 'rgba(255,99,132,1)',
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.4)',
+            'rgba(54, 162, 235, 0.4)',
+            'rgba(255, 206, 86, 0.4)',
+            'rgba(75, 192, 192, 0.4)',
+            'rgba(153, 102, 255, 0.4)',
+            'rgba(255, 159, 64, 0.4)'
+            ],
+          borderWidth: 1,
+          data: data
+        }
+      ]
+  }
+}
+
+const chartOptions = {
+  maintainAspectRatio: false,
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero:true,
+        callback: function(value, index, values) {
+          return value.toLocaleString('en');
+        },
+      }
+    }]
+  },
+  legend: false,
+  plugins: {
+    datalabels: {
+      formatter: (value, ctx) => {
+        return value !== 0 ?
+          value.toLocaleString('id') :
+          'N/A'
+      },
+      anchor: 'end',
+      align: 'start',
+      color: 'gray',
+      font: {
+        size: 20,
+        style: bold
+      }
+    }
+  }
+}
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      title: "Penerimaan Iuran"
+      title: "Penerimaan Iuran",
+      iuranData: {chartData: {}},
+      tkData: {chartData: {}},
+      nppData: { chartData: {}}
     }
   }
+
+  componentWillMount() {
+    this.generateIuran()
+  }
+
+  generateIuran = () => {
+    this.setState({ title: "Penerimaan Iuran"})
+    axios.get('http://127.0.0.1:8000/api/iuran/')
+    .then(res => {
+      const allIurans = res.data
+      let iuran = []
+      let bulan = []
+      allIurans.forEach(allIuran => {
+        iuran.push(allIuran.jml_bayar)
+        bulan.push(moment(allIuran.month).locale('id', localization).format('MMMM'))
+      })
+      const add = (a, b) => a + b
+      const total_iuran = iuran.reduce(add)
+      const lastMonth = bulan[bulan.length - 1]
+      const lastIuran = iuran[iuran.length - 1]
+      const chartData = createChartData(bulan, "Penerimaan Iuran", iuran)
+      this.setState({iuranData: {total_iuran, lastMonth, lastIuran, chartData, chartOptions}})
+    })
+  }
+
   render() {
     const settings = {
       dots: true,
@@ -37,13 +123,47 @@ class App extends Component {
       beforeChange: (currentIndex, nextIndex) => {
         switch(nextIndex) {
           case 0:
-            this.setState({ title: "Penerimaan Iuran"})
+            this.generateIuran()      
             break
           case 1:
             this.setState({ title: "Penambahan TK"})
+            axios.get('http://127.0.0.1:8000/api/tk/')
+            .then(res => {
+              const allTks = res.data
+              let jumlah = []
+              let bulan = []
+              allTks.forEach(allTk => {
+                jumlah.push(allTk.jml_tk)
+                bulan.push(moment(allTk.month).locale('id', localization).format('MMMM'))
+              })
+              const add = (a, b) => a + b
+              const totalTk = jumlah.reduce(add)
+              const lastMonth = bulan[bulan.length - 1]
+              const lastTk = jumlah[jumlah.length - 1]
+              const chartData = createChartData(bulan, "Penambahan TK", jumlah)
+              this.setState({tkData: {totalTk, lastMonth, lastTk, chartData, chartOptions}})
+            })
             break
           case 2:
             this.setState({ title: "Akuisisi NPP"})
+            axios.get('http://127.0.0.1:8000/api/npp/')
+            .then(res => {
+              const allNpp = res.data
+              const allPenambahan = allNpp['penambahan_npp']
+              let npp = []
+              let bulan = []
+              allPenambahan.forEach(penambahan => {
+                npp.push(penambahan.jml_npp)
+                bulan.push(moment(penambahan.month).locale('id', localization).format('MMMM'))
+              })
+              const add = (a, b) => a + b
+              const totalPenambahan = npp.reduce(add)
+              const lastMonth = bulan[bulan.length - 1]
+              const lastNpp = npp[npp.length - 1]
+              const chartData = createChartData(bulan, "Akuisisi NPP", npp)
+              console.log(chartData)
+              this.setState({nppData: {totalPenambahan, lastMonth, lastNpp, chartData, chartOptions}})
+            })
             break
           default:
             this.setState({ title: "Dashboard Pekanbaru Kota"})
@@ -75,13 +195,13 @@ class App extends Component {
         <div className="App">
           <Slider {...settings}>
             <div>
-              <TotalIuran />
+              <TotalIuran {...this.state.iuranData}/>
             </div>
             <div>
-              <TenagaKerja />
+              <TenagaKerja {...this.state.tkData}/>
             </div>
             <div>
-              <Npp />
+              <Npp {...this.state.nppData} />
             </div>
           </Slider>
         </div>
